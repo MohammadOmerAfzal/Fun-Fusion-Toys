56c3f98be42381d3da5028b18de6bb87264fd5ba
MohammadOmerAfzal <m.omarafzal12@gmail.com>
2025-11-03 22:32:30 +0500

    vite-config-fixed

--- frontend/ToyStore/vite.config.js
+++ frontend/ToyStore/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+  host: true,   // listens on all interfaces (0.0.0.0)
+  port: 5173,   // container internal port
+}
 })
+
